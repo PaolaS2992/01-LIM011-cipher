53e2f7b054ffa910e86e7e746141eee1b2efd0e5
PaolaS2992 <huaripayta29.92@gmail.com>
2019-10-07 20:45:43 -0500

    Primer Sprint - Laboratoria V2 - Separando logica cipher.js del DOM index.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,49 +1,33 @@
-window.addEventListener('load', () => {
 
-/* CAPTURAR LOS ELEMENTOS DE HTML*/
+window.cipher = {
+    // Acá escribe tu código.
 
-// ELEMENTOS PARA ENCRIPTAR.
-let txtNroEncriptar = document.getElementById('txtNroEncriptar');
-let txtEncriptar = document.getElementById('txtEncriptar');
-let btnEncriptar = document.getElementById('btnEncriptar');
+    //FUNCION ENCRIPTAR.
+    encode: (offset, string) => {
 
-// ELEMENTOS PARA DESENCRIPTAR.
-let txtNroDesencriptar = document.getElementById('txtNroDesencriptar');
-let txtDesencriptar = document.getElementById('txtDesencriptar');
-let btnDesencriptar = document.getElementById('btnDesencriptar');
-
-// ELEMENTO PARA MOSTRAR RESPUESTA.
-let txtRespuesta1 = document.getElementById('txtRespuesta1');
-let txtRespuesta2 = document.getElementById('txtRespuesta2');
-
-
-// ********** FUNCIÓN ENCRIPTAR **********
-
-btnEncriptar.addEventListener('click', () => {
-
-    let cadena = txtEncriptar.value;
-    let posicion = parseInt(txtNroEncriptar.value); 
+    //let cadena = txtEncriptar.value;
+    //let posicion = parseInt(txtNroEncriptar.value); 
 
     let txtEncriptado = '';
 
-        for(let i = 0; i < cadena.length; i++){
+        for(let i = 0; i < string.length; i++){
         //LA VARIABLE QUE CONVIERTE LA LETRA EN CODIGO ASCCI.
-        let code = cadena.charCodeAt(i);
+        let code = string.charCodeAt(i);
         console.log(code);
 
         //LA VARIABLE QUE GUARDA LA CONVERSION DE CODIGO ASCCI A LETRA.
-        let code2 = cadena[i];
+        let code2 = string[i];
         console.log(code2);
 
-        if(posicion > 0){
+        if(offset > 0){
             //ANALIZANDO LAS LETRAS MAYUSCULAS A = 65 y Z = 90
             if(code >= 65 && code <= 90){
             //INGRESAR EL NRO DE ESPACIOS Y CONVERTIRLO DE LENGUAJE ASCCI A LETRA.
-                code2 = String.fromCharCode((code - 65 + posicion) % 26 + 65);
+                code2 = String.fromCharCode((code - 65 + offset) % 26 + 65);
                 console.log("nueva letra: ",code2);
             //ANALIZANDO LAS LETRAS MINUSCULAS a = 97 y z = 122
             }else if(code >= 97 && code <= 122){
-                code2= String.fromCharCode((code - 97 + posicion) % 26 + 97);
+                code2= String.fromCharCode((code - 97 + offset) % 26 + 97);
                 console.log("Nueva letra: ", code2);
             }else{
                 alert("SOLO INGRESA LETRAS");
@@ -60,37 +44,38 @@ btnEncriptar.addEventListener('click', () => {
         }
 
         //IMPRIMIR RESPUESTA.
-        txtRespuesta1.innerHTML = "El texto encriptado es: " + txtEncriptado;       
-});
+        //txtRespuesta1.innerHTML = "El texto encriptado es: " + txtEncriptado;    
 
-// ********** FUNCIÓN DESENCRIPTAR **********
+        return txtEncriptado;
+    },
 
-btnDesencriptar.addEventListener('click', () =>{
+    //FUNCION DESENCRIPTAR.
+    decode: (offset, string) => {
 
-    let cadena = txtDesencriptar.value;
-    let posicion = parseInt(txtNroDesencriptar.value);
+    //let cadena = txtDesencriptar.value;
+    //let posicion = parseInt(txtNroDesencriptar.value);
 
     let txtDescriptado='';
 
-    for(let i = 0; i < cadena.length; i++){
+    for(let i = 0; i < string.length; i++){
       
       //LA VARIABLE QUE CONVIERTE LA LETRA EN CODIGO ASCII
-      let code = cadena.charCodeAt(i);
+      let code = string.charCodeAt(i);
       console.log(code);
   
       //LA VARIABLE QUE GUARDA LA CONVERSIÓN DE CODIGO ASCCI A LETRA.
-      let code2 = cadena[i];
+      let code2 = string[i];
       console.log(code2);
         
-    if(posicion >= 0){
+    if(offset >= 0){
       //ANALIZANDO LAS LETRAS MAYUSCULAS A = 65 y Z = 90
         if(code >= 65 && code <= 90){
             //INGRESAR EL NRO DE ESPACIOS Y CONVERTIRLO DE LENGUAJE ASCCI A LETRA.
-            code2 = String.fromCharCode(((code - 65 - posicion + 26 * 2)% 26) + 65);
+            code2 = String.fromCharCode(((code - 65 - offset + 26 * 2)% 26) + 65);
             console.log(code2);
       //ANALIZANDO LAS LETRAS MINUSCULAS a = 97 y z = 122
         }else if(code >= 97 && code <= 122){
-            code2 = String.fromCharCode(((code - 97 - posicion + 26 * 2)% 26) + 97);
+            code2 = String.fromCharCode(((code - 97 - offset + 26 * 2)% 26) + 97);
             console.log(code2);
         }else{
             alert("SOLO INGRESA LETRAS");
@@ -105,8 +90,11 @@ btnDesencriptar.addEventListener('click', () =>{
       console.log("resutado",txtDescriptado);
   
     }
-    txtRespuesta2.innerHTML = "El texto desencriptado es: " + txtDescriptado;
-});
+    //txtRespuesta2.innerHTML = "El texto desencriptado es: " + txtDescriptado;
 
+        return txtDescriptado;
+    }
 
-});
+    
+};
+  
--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
-// CAPTURAMOS LOS DIV ATRAVEZ DE ID.
+// CAPTURAR LOS ELEMENTOS DE HTML PARA LAS FUNCIONES
+
+// ELEMENTOS PARA ENCRIPTAR.
+let txtNroEncriptar = document.getElementById('txtNroEncriptar');
+let txtEncriptar = document.getElementById('txtEncriptar');
+let btnEncriptar = document.getElementById('btnEncriptar');
+
+// ELEMENTOS PARA DESENCRIPTAR.
+let txtNroDesencriptar = document.getElementById('txtNroDesencriptar');
+let txtDesencriptar = document.getElementById('txtDesencriptar');
+let btnDesencriptar = document.getElementById('btnDesencriptar');
+
+// ELEMENTO PARA MOSTRAR RESPUESTA.
+let txtRespuesta1 = document.getElementById('txtRespuesta1');
+let txtRespuesta2 = document.getElementById('txtRespuesta2');
+
+// BOTON EVENTO CLICK "CIFRA".
+btnEncriptar.addEventListener("click", () => {
+    txtRespuesta1.innerHTML = window.cipher.encode(parseInt(txtNroEncriptar.value),txtEncriptar.value);
+});
+
+// BOTONN EVENTO CLICK "DESCIFRA".
+btnDesencriptar.addEventListener("click", () => {
+    txtRespuesta2.innerHTML = window.cipher.decode(parseInt(txtNroDesencriptar.value),txtDesencriptar.value);
+});
+
+// CAPTURAMOS LOS ID DEL DIV PARA MOSTRAR Y OCULTAR.
 
 const btnSgte = document.getElementById("btnSgte");
 const inicio = document.getElementById("inicio");
 const primeraVista = document.getElementById("primeraVista");
 
-let txtEncriptar = document.getElementById("txtEncriptar");
-let txtNroEncriptar = document.getElementById("txtNroEncriptar");
-//let txtRespuesta1 = document.getElementById("txtRespuesta1");
+// BOTON SIGUIENTE.
 
 btnSgte.addEventListener("click",() => {
 
@@ -25,9 +49,7 @@ btnSgte.addEventListener("click",() => {
 const btnFin = document.getElementById("btnFin");
 const segundaVista = document.getElementById("segundaVista");
 
-let txtDesencriptar = document.getElementById('txtDesencriptar');
-let txtNroDesencriptar = document.getElementById('txtNroDesencriptar');
-
+// BOTON FINALIZAR.
 btnFin.addEventListener("click",() => {
 
     cadena = txtDesencriptar.value;
