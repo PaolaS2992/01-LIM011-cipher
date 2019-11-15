301beb5d15c646032eedbbbfa78742404c9100d5
PaolaS2992 <huaripayta29.92@gmail.com>
2019-11-15 17:13:03 -0500

    test de usabilidad

---
+++ src/data.js
@@ -0,0 +1,65 @@
+// Minidata
+export const traerDataMap2 = (arr) => arr.map((ele) => {
+  const obj = {
+    identificador: ele.id,
+    nombre: ele.name,
+    imagen: ele.img,
+    numero: ele.num,
+    tipo: ele.type,
+    altura: ele.height,
+    peso: ele.weight,
+    caramelos: ele.candy_count,
+    horaAparicion: ele.spawn_time,
+    debilidades: ele.weaknesses,
+    huevo: ele.egg,
+    frecuencia: ele.avg_spawns,
+  };
+  return obj;
+});
+// Filtro Huevos
+export const filtroHuevo = (array, distancia) => {
+  const nuevoArray = array.filter((objeto) => objeto.huevo === distancia);
+  return nuevoArray;
+};
+// Filtro debilidades y tipo
+export const filtroDebilidadTipo = (array, propiedad, valor) => {
+  const newArray = [];
+  array.forEach((objeto) => {
+    objeto[propiedad].forEach((string) => {
+      if (string === valor) {
+        newArray.push(objeto);
+      }
+    });
+  });
+  return newArray;
+};
+
+// filtro buscador por nombre
+export const buscarPorNombre = (array, str) => {
+  const nuevoArraysss = array.filter((objeto) => objeto.nombre.toLowerCase().startsWith(str));
+  return nuevoArraysss;
+};
+
+// Filtro orden alfabetico, orden asc - desc y top
+// eslint-disable-next-line arrow-body-style
+export const ordenAlfNum = (arr, propiedad) => {
+  return arr.sort((a, b) => (a[propiedad] < b[propiedad] ? -1 : 1));
+};
+
+// SELECCIONANDO STRING.
+// eslint-disable-next-line consistent-return
+export const AsDes = (data, string) => {
+  switch (string) {
+    case 'A-Z':
+      return ordenAlfNum(data, 'nombre');
+    case 'Z-A':
+      return ordenAlfNum(data, 'nombre').reverse();
+    case 'ASC':
+      return ordenAlfNum(data, 'numero');
+    case 'DESC':
+      return ordenAlfNum(data, 'numero').reverse();
+    case 'MAYORF':
+      return ordenAlfNum(data, 'frecuencia').slice(0, 10);
+    // no default
+  }
+};
---
+++ src/main.js
@@ -0,0 +1,100 @@
+import POKEMON from './data/pokemon/pokemon.js';
+import {
+  traerDataMap2, filtroHuevo, filtroDebilidadTipo, buscarPorNombre, AsDes,
+} from './data.js';
+
+const card = (obj) => {
+  const divElement = document.createElement('div');
+  divElement.classList.add('tarjeta-pokemon');
+  divElement.innerHTML = `
+    <img src="${obj.imagen}" class="imagen-pokemon">
+    <p class="nombre-pokemon">${obj.nombre}</p>
+    <p class="numero-pokemon">${obj.numero} </p>  
+  `;
+
+  divElement.addEventListener('click', () => {
+    const divElementModal = document.createElement('div');
+    divElementModal.classList.add('modal'); // Agrego el atributo class="modal"
+    divElementModal.innerHTML = `
+      <div class="flex">
+        <div class="contenido-modal">
+          <span class="cerrar" id="cerrar">&times;</span>
+          <div class"modal-body">
+            <img src="${obj.imagen}" class="datos-alternativos-img">
+            <div class="datos-alternativos">Tipo: ${obj.tipo}</div>
+            <div class="datos-alternativos">Altura: ${obj.altura}</div>
+            <div class="datos-alternativos">Peso: ${obj.peso}</div>
+            <div class="datos-alternativos">Caramelos: ${obj.caramelos}</div>
+            <div class="datos-alternativos">Hora de aparación: ${obj.horaAparicion}</div>
+            <div class="datos-alternativos">Debilidades:</div>
+            <div class="datos-alternativos">${obj.debilidades}</div>
+            <figure class="datos-alternativos">
+              <img src="./imagenes/huevo.png" width="15" height"15">${obj.huevo}
+            </figure>
+          </div>
+        </div>
+      </div>
+    `;
+    document.body.appendChild(divElementModal);
+    divElementModal.classList.add('modal-open');
+    const cerrar = document.getElementById('cerrar');
+    // Cerrar modal.
+    cerrar.addEventListener('click', () => {
+      document.body.removeChild(divElementModal);
+    });
+  });
+  return divElement;
+};
+
+const seccionCardsPokemones = document.querySelector('#insertar-pokemones');
+const dataPokemon = traerDataMap2(POKEMON);
+const templateCard = (arr) => {
+  seccionCardsPokemones.innerHTML = '';
+  arr.forEach((obj) => {
+    seccionCardsPokemones.appendChild(card(obj));
+  });
+};
+templateCard(dataPokemon);
+
+
+//  FILTRO POR HUEVO.
+document.querySelector('#filtro-distancia').addEventListener('change', () => {
+  const seleccionarhuevo = document.querySelector('#filtro-distancia').value;
+  templateCard(filtroHuevo(dataPokemon, seleccionarhuevo));
+});
+// FILTRO POR DEBILIDADES.
+document.querySelector('#filtro-debilidades').addEventListener('change', () => {
+  const seleccionarDebilidad = document.querySelector('#filtro-debilidades').value;
+  templateCard(filtroDebilidadTipo(dataPokemon, 'debilidades', seleccionarDebilidad));
+});
+
+// FILTRO POR TIPO
+document.querySelector('#guia-tipos').addEventListener('click', (event) => {
+  const seleccionarTipo = event.target.alt;
+  templateCard(filtroDebilidadTipo(dataPokemon, 'tipo', seleccionarTipo));
+});
+
+// FILTRO POR ORDEN ALFABETICO Y NUMERICO.
+document.querySelector('#ordenAlfNum').addEventListener('change', () => {
+  const seleccionOpcion = document.querySelector('#ordenAlfNum').value;
+  templateCard(AsDes(dataPokemon, seleccionOpcion));
+});
+
+// TOP 10 DE FRECUENCIA DE APARICIÓN.
+const btnTop10 = document.querySelector('#mayor-frecuencia');
+btnTop10.addEventListener('click', () => {
+  templateCard(AsDes(dataPokemon, btnTop10.value));
+});
+
+//  FILTRO BUSCADOR
+document.querySelector('#nombre-pokemon').addEventListener('input', (event) => {
+  const pokemonBuscado = event.target.value.toLowerCase();
+  templateCard(buscarPorNombre(dataPokemon, pokemonBuscado));
+});
+
+// MENU
+const toglee = document.querySelector('.toglee');
+toglee.addEventListener('click', () => {
+  document.getElementById('siderbar').classList.toggle('active');
+  document.querySelector('#filtros').reset();
+});
