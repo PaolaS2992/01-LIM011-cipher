b3b362ff7f44064aa39525ee12f583feba501891
PaolaS2992 <huaripayta29.92@gmail.com>
2019-12-03 09:46:42 -0500

    Con Eslint

--- src/data.js
+++ src/data.js
@@ -1,8 +1,8 @@
+/* eslint-disable no-param-reassign */
 // FUNCION MINIDATA.
 export const traerDataMap2 = (arr) => arr.map((ele) => {
   const obj = {
     identificador: ele.id,
-    debilidades: ele.weaknesses,
     nombre: ele.name,
     imagen: ele.img,
     numero: ele.num,
@@ -10,11 +10,12 @@ export const traerDataMap2 = (arr) => arr.map((ele) => {
     altura: ele.height,
     peso: ele.weight,
     caramelos: ele.candy_count,
-    horaAparicion: ele.spawn_time,    
+    horaAparicion: ele.spawn_time,
+    debilidades: ele.weaknesses,
     huevo: ele.egg,
     frecuencia: ele.avg_spawns,
     Previo: ele.prev_evolution,
-    Siguiente: ele.next_evolution,    
+    Siguiente: ele.next_evolution,
   };
   return obj;
 });
@@ -25,67 +26,28 @@ export const filtroHuevo = (array, distancia) => {
 };
 // FUNCION PARA DEBILIDADES Y TIPO.
 export const filtroDebilidadTipo = (array, propiedad, valor) => {
-  const newArray = []
+  const newArray = [];
   array.forEach((objeto) => {
     objeto[propiedad].forEach((string) => {
-      if(string === valor) { 
+      if (string === valor) {
         newArray.push(objeto);
       }
     });
   });
   return newArray;
 };
-// FILTRO AVANZADO: SE CONSIDERA EL TIPO Y DEBILIDAD.
-export const filtroFusionado = (arr, tipo, debilidad) => {
-  const newArray = [];
-  const newArray1 = [];
-  arr.forEach((objeto) => {
-    objeto.tipo.forEach((string) => {
-      if(string === tipo){
-        newArray.push(objeto);   
-      }   
-    });  
-  });
-  newArray.forEach((objeto) => {
-    objeto.debilidades.forEach((string) => {
-      if(string === debilidad){
-        newArray1.push(objeto);
-      }
-    });
-  });
-  return newArray1;
-}
-// FUNCION PARA ORDENAR DEL MENOS Y MÁS DEBIL.
-export const filtroGeneralDebilidad = (arr, str) => {
-  let newArray = [];
-  if(str === 'MENOSDEBI'){
-    newArray = arr.sort((a, b) => {
-      if(a.debilidades.length > b.debilidades.length) return 1;
-      if(a.debilidades.length < b.debilidades.length) return -1;
-      return 0;
-    });
-  }
-  if(str === 'MASDEBI'){
-    newArray = arr.sort((a, b) => {
-      if(b.debilidades.length > a.debilidades.length) return 1;
-      if(b.debilidades.length < a.debilidades.length) return -1;
-      return 0;
-    });
-  }
-  return newArray;
-}
 // FUNCION PARA BUSCAR NOMBRES.
-export const buscarPorNombre = (array, nombre) => {
-  const nuevoArraysss = array.filter((objeto) => objeto.nombre.toLowerCase().startsWith(nombre));
+export const buscarPorNombre = (array, nombres) => {
+  const nuevoArraysss = array.filter((objeto) => objeto.nombre.toLowerCase().startsWith(nombres));
   return nuevoArraysss;
 };
 // FUNCION PARA ORDENAR POR NOMBRE, NUMERO Y TOP 10
+// eslint-disable-next-line arrow-body-style
 export const ordenAlfNum = (arr, propiedad) => {
-  return arr.sort((a, b) => {
-    if (a[propiedad] < b[propiedad]){return -1;}
-  });
+  return arr.sort((a, b) => (a[propiedad] > b[propiedad] ? 1 : -1));
 };
 // SELECCIONANDO STRING.(PARTE DE LA FUNCIÓ ORDENAR).
+// eslint-disable-next-line consistent-return
 export const AsDes = (data, string) => {
   switch (string) {
     case 'A-Z':
@@ -97,21 +59,20 @@ export const AsDes = (data, string) => {
     case 'DESC':
       return ordenAlfNum(data, 'numero').reverse();
     case 'MENORF':
-      return ordenAlfNum(data, 'frecuencia').slice(0,10);
+      return ordenAlfNum(data, 'frecuencia').slice(0, 10);
     case 'MAYORF':
-      return ordenAlfNum(data, 'frecuencia').reverse().slice(0,10);
-    default:
-      break;
+      return ordenAlfNum(data, 'frecuencia').reverse().slice(0, 10);
+      // no default
   }
-}
+};
 // FUNCION PARA EVOLUCIONES.
 export const evolutions = (arr, propiedad, num) => {
   let newArray = [];
   arr.forEach((objeto) => {
-    if(objeto.identificador === num){
-      if(objeto[propiedad] === undefined){
+    if (objeto.identificador === num) {
+      if (objeto[propiedad] === undefined) {
         newArray = [];
-      }else{
+      } else {
         objeto[propiedad].forEach((obj) => {
           obj.label = propiedad;
           newArray.push(obj);
@@ -120,9 +81,48 @@ export const evolutions = (arr, propiedad, num) => {
     }
   });
   return newArray;
-}
+};
 // FUNCION PARA INF.EVOLUCIONES Y CARAMELOS.
 export const filterInfEvoAndCandy = (arr, propiedad, string) => {
-  const newArray = arr.filter(obj => obj[propiedad] === string);
+  const newArray = arr.filter((obj) => obj[propiedad] === string);
   return newArray;
-}
+};
+// FUNCION PARA ORDENAR EL MENOS Y MÁS DEBIL.
+export const filtroGeneralDebilidad = (arr, str) => {
+  let newArray = [];
+  if (str === 'MENOSDEBI') {
+    newArray = arr.sort((a, b) => {
+      if (a.debilidades.length > b.debilidades.length) return 1;
+      if (a.debilidades.length < b.debilidades.length) return -1;
+      return 0;
+    });
+  }
+  if (str === 'MASDEBI') {
+    newArray = arr.sort((a, b) => {
+      if (b.debilidades.length > a.debilidades.length) return 1;
+      if (b.debilidades.length < a.debilidades.length) return -1;
+      return 0;
+    });
+  }
+  return newArray;
+};
+// FILTRO AVANZADO: SE CONSIDERA EL TIPO Y DEBILIDAD.
+export const filtroFusionado = (arr, tipo, debilidad) => {
+  const newArray = [];
+  const newArray1 = [];
+  arr.forEach((objeto) => {
+    objeto.tipo.forEach((string) => {
+      if (string === tipo) {
+        newArray.push(objeto);
+      }
+    });
+  });
+  newArray.forEach((objeto) => {
+    objeto.debilidades.forEach((string) => {
+      if (string === debilidad) {
+        newArray1.push(objeto);
+      }
+    });
+  });
+  return newArray1;
+};
--- src/main.js
+++ src/main.js
@@ -1,22 +1,64 @@
 import POKEMON from './data/pokemon/pokemon.js';
 import {
-  traerDataMap2, filtroHuevo, filtroDebilidadTipo, buscarPorNombre, AsDes, evolutions, filterInfEvoAndCandy, filtroFusionado, filtroGeneralDebilidad, 
+  traerDataMap2, filtroHuevo, filtroDebilidadTipo, buscarPorNombre, AsDes,
+  evolutions, filterInfEvoAndCandy, filtroFusionado, filtroGeneralDebilidad,
 } from './data.js';
 
 // MINIDATA.
 const dataPokemon = traerDataMap2(POKEMON);
-
 // FUNCION PARA CREAR EL STRING TEMPLATE POKEMONES.
 const strTemplatePokemon = (obj) => {
-  let newString = `<img src="${obj.imagen}" class="imagen-pokemon">
+  const newString = `<img src="${obj.imagen}" class="imagen-pokemon">
   <p class="nombre-pokemon">${obj.nombre}</p>
   <p class="numero-pokemon">${obj.numero}</p>  
   </div>`;
   return newString;
-}
+};
+// FUNCION PARA CREAR EL STRING TEMPLATE DE INFORMACION DE EVOLUCIONES.
+const strTemplateInfoEvolution = (arr) => {
+  let newStringInfo = '';
+  arr.forEach((obj) => {
+    newStringInfo += `        
+          <div>
+              <span>Tipo: </span>
+              <span>${obj.tipo}</span>
+          </div>
+          <div>
+              <span>Huevo: </spam>
+              <span>${obj.huevo}</span>
+          </div>
+          <div>
+              <span>Hora aparición: </span>
+              <span>${obj.horaAparicion}</span>
+          </div>
+      `;
+  });
+  return newStringInfo;
+};
+// FUNCION PARA CREAR EL STRING TEMPLATE EVOLUCIONES.
+const strTemplateEvolution = (arr) => {
+  let newString = '';
+  arr.forEach((obj) => {
+    newString += `
+      <div class = "card-evolucion">
+          <div class="lado-front">
+              <p>${obj.label} Evolución</p>
+              <h5>${obj.num} - ${obj.name}</h5>
+              <figure>
+                  <img src="http://www.serebii.net/pokemongo/pokemon/${obj.num}.png" alt="" class="imagen">
+              </figure>
+          </div>
+          <div class="lado-back">
+              ${strTemplateInfoEvolution(filterInfEvoAndCandy(dataPokemon, 'numero', obj.num))}
+          </div>
+      </div>
+      `;
+  });
+  return newString;
+};
 // FUNCION PARA CREAR EL STRING TEMPLATE MODAL.
 const strTemplateModal = (obj) => {
-  let newString = `
+  const newString = `
 <div class="flex">
   <div class="contenido-modal">
     <span class="cerrar" id="cerrar">&times;</span>
@@ -79,69 +121,27 @@ const strTemplateModal = (obj) => {
   </div>
 </div>`;
   return newString;
-}
-// FUNCION PARA CREAR EL STRING TEMPLATE EVOLUCIONES.
-const strTemplateEvolution = (arr) => {
-  let newString = '';
-  arr.forEach((obj) => {
-      newString += `
-      <div class = "card-evolucion">
-          <div class="lado-front">
-              <p>${obj.label} Evolución</p>
-              <h5>${obj.num} - ${obj.name}</h5>
-              <figure>
-                  <img src="http://www.serebii.net/pokemongo/pokemon/${obj.num}.png" alt="" class="imagen">
-              </figure>
-          </div>
-          <div class="lado-back">
-              ${strTemplateInfoEvolution(filterInfEvoAndCandy(dataPokemon,'numero', obj.num))}
-          </div>
-      </div>
-      `;
-  });
-  return newString;
-}
-// FUNCION PARA CREAR EL STRING TEMPLATE DE INFORMACION DE EVOLUCIONES.
-const strTemplateInfoEvolution = (arr) => {
-  let newStringInfo = '';
-  arr.forEach((obj) => {
-      newStringInfo += `        
-          <div>
-              <span>Tipo: </span>
-              <span>${obj.tipo}</span>
-          </div>
-          <div>
-              <span>Huevo: </spam>
-              <span>${obj.huevo}</span>
-          </div>
-          <div>
-              <span>Hora aparición: </span>
-              <span>${obj.horaAparicion}</span>
-          </div>
-      `;
-  });
-  return newStringInfo;
-}
+};
 // FUNCION PARA CREAR CONTENEDOR DE TODOS LOS STRING TEMPLATE.
 const card = (obj) => {
   const divElement = document.createElement('div');
-  divElement.classList.add("tarjeta-pokemon");
+  divElement.classList.add('tarjeta-pokemon');
   divElement.innerHTML = strTemplatePokemon(obj);
   // MODAL AL HACER CLICK.
-  divElement.addEventListener('click', () => {    
-    const divElementModal = document.createElement('div'); 
-    divElementModal.classList.add("modal"); // Agrego el atributo class="modal"
-    divElementModal.innerHTML = strTemplateModal(obj);   
+  divElement.addEventListener('click', () => {
+    const divElementModal = document.createElement('div');
+    divElementModal.classList.add('modal'); // Agrego el atributo class="modal"
+    divElementModal.innerHTML = strTemplateModal(obj);
     document.body.appendChild(divElementModal); // Agrego el div en todo el BODY.
-    divElementModal.classList.add('modal-open');     
+    divElementModal.classList.add('modal-open');
     // Cerrar modal.
     const cerrar = document.getElementById('cerrar');
     cerrar.addEventListener('click', () => {
-     document.body.removeChild(divElementModal); 
+      document.body.removeChild(divElementModal);
     });
   });
   return divElement;
-}
+};
 // FUNCION PARA RECORRER Y AGREGAR LOS NODOS.
 const seccionCardsPokemones = document.querySelector('#insertar-pokemones');
 const templateCard = (arr) => {
@@ -160,24 +160,23 @@ document.querySelector('#filtro-distancia').addEventListener('change', () => {
 // FILTRO POR DEBILIDADES.
 document.querySelector('#filtro-debilidades').addEventListener('change', () => {
   const seleccionarDebilidad = document.querySelector('#filtro-debilidades').value;
-  templateCard(filtroDebilidadTipo(dataPokemon, 'debilidades' ,seleccionarDebilidad));
+  templateCard(filtroDebilidadTipo(dataPokemon, 'debilidades', seleccionarDebilidad));
 });
 // FILTRO POR TIPO.
 document.querySelector('#guia-tipos').addEventListener('click', (event) => {
   const seleccionarTipo = event.target.alt;
-  templateCard(filtroDebilidadTipo(dataPokemon, 'tipo' , seleccionarTipo));
+  templateCard(filtroDebilidadTipo(dataPokemon, 'tipo', seleccionarTipo));
 });
 // FILTRO POR ORDEN ALFABETICO Y NUMERICO.
-document.querySelector('#ordenAlfNum').addEventListener('change', ()=>{
+document.querySelector('#ordenAlfNum').addEventListener('change', () => {
   const seleccionOpcion = document.querySelector('#ordenAlfNum').value;
   templateCard(AsDes(dataPokemon, seleccionOpcion));
 });
 // FILTRO DEL MENOS AL MÁS DÉBIL.
 document.querySelector('#filtro-mas-menos-debil').addEventListener('click', (e) => {
   const selecccionDebil = e.target.id;
-  console.log(selecccionDebil)
   templateCard(filtroGeneralDebilidad(dataPokemon, selecccionDebil));
-})
+});
 // TOP 10 DE FRECUENCIA DE APARACIÓN.
 document.querySelector('#filtro-frecuencia').addEventListener('click', (e) => {
   const selectFrecuencia = e.target.id;
@@ -191,7 +190,7 @@ document.querySelector('#nombre-pokemon').addEventListener('input', (e) => {
 // FILTRO CANTIDAD DE CANDYS.
 document.querySelector('#filtro-candys').addEventListener('click', (e) => {
   const seleccionRadio = e.target.id;
-  templateCard(filterInfEvoAndCandy(dataPokemon,'caramelos', parseInt(seleccionRadio)));
+  templateCard(filterInfEvoAndCandy(dataPokemon, 'caramelos', parseInt(seleccionRadio, 10)));
 });
 // MENU TOGLEE.
 const toglee = document.querySelector('.toglee');
@@ -213,6 +212,7 @@ btnMostrar.addEventListener('click', () => {
 });
 // 1.2. FILTRO AVANZADO - BOTON OCULTAR.
 const btnOcultar = document.querySelector('#btn-ocultar');
+const divMensaje = document.querySelector('.mensaje');
 btnOcultar.addEventListener('click', () => {
   // A. APLICAR DISPLAY NONE Y BLOCK RESPECTIVAMENTE.
   divFilterAvanzado.style.display = 'none';
@@ -224,12 +224,13 @@ btnOcultar.addEventListener('click', () => {
   // B.2. VOLVER AL CHECK FALSE. LIMPIAR CHECK
   const inputTipo = document.querySelectorAll('input.tipo');
   inputTipo.forEach((ele) => {
+    // eslint-disable-next-line no-param-reassign
     ele.checked = false;
   });
   // B.3. LIMPIAR DIV MENSAJE.
   divMensaje.style.display = 'none';
   // B.4. VOLVER A ORDENAR LOS POKEMONES.
-  templateCard(AsDes(dataPokemon, 'ASC')); 
+  templateCard(AsDes(dataPokemon, 'ASC'));
 });
 
 // 2. *** FILTRO AVANZADO - FUNCIONES ***
@@ -237,45 +238,42 @@ btnOcultar.addEventListener('click', () => {
 const infoTipo = (arr) => {
   let tipo = '';
   arr.forEach((ele) => {
-    if(ele.checked === true){
+    if (ele.checked === true) {
       tipo = ele.id;
     }
   });
   return tipo;
-}
+};
 // 2.2. FUNCION PARA CAPTURAR EL "SELECTED" SELECCIONADO.
 const infoDebilidad = (arr) => {
   let debilidad = '';
   arr.forEach((ele) => {
-    if(ele.selected === true){
+    if (ele.selected === true) {
       debilidad = ele.value;
     }
   });
   return debilidad;
-}
+};
 // 3. *** FILTRO AVANZADO - BOTON FILTRAR ***
 const btnFiltrar = document.querySelector('#btn-filtrar');
-const divMensaje = document.querySelector('.mensaje');
 btnFiltrar.addEventListener('click', () => {
   // A. CHECKED TIPO SELECCIONADO.
   const inputTipo = document.querySelectorAll('input.tipo');
   const checkTipo = infoTipo(inputTipo);
-  console.log('Info Tipo:', checkTipo);
   // B. SELECTED DEBILIDAD SELECCIONADO.
   const optionDebilidades = document.querySelectorAll('option.debilidad');
   const selectDebilidad = infoDebilidad(optionDebilidades);
-  console.log('Info Debilidades:',selectDebilidad);
   // C. VALIDACIÓN DE CAMPOS SELECCIONADOS - (DE ACUERDO A ELLO SE COLOCARÁ LA FUNCIÓN).
-  if(checkTipo === '' && selectDebilidad !== ''){
+  if (checkTipo === '' && selectDebilidad !== '') {
     templateCard(filtroDebilidadTipo(dataPokemon, 'debilidades', selectDebilidad));
     divMensaje.style.display = 'none';
-  } else if (checkTipo !== '' && selectDebilidad === ''){
+  } else if (checkTipo !== '' && selectDebilidad === '') {
     templateCard(filtroDebilidadTipo(dataPokemon, 'tipo', checkTipo));
     divMensaje.style.display = 'none';
-  } else if (checkTipo !== '' && selectDebilidad !== ''){
+  } else if (checkTipo !== '' && selectDebilidad !== '') {
     templateCard(filtroFusionado(dataPokemon, checkTipo, selectDebilidad));
     divMensaje.style.display = 'none';
-  } else {    
+  } else {
     divMensaje.style.display = 'block';
   }
 });
@@ -288,6 +286,7 @@ btnLimpiar.addEventListener('click', () => {
   // B. VOLVER AL CHECK FALSE. LIMPIAR CHECK
   const inputTipo = document.querySelectorAll('input.tipo');
   inputTipo.forEach((ele) => {
+    // eslint-disable-next-line no-param-reassign
     ele.checked = false;
   });
   // C. LIMPIAR DIV MENSAJE.
